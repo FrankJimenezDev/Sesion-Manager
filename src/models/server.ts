cec4df349c45abfc3bfbc6b771180c82afbe2d8a
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-27 12:20:56 -0500

    inicializacion de la base de datos en la clase server

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,5 +1,7 @@
 import "dotenv/config"
 import express, { Application } from "express"
+import cors from "cors"
+import { db } from '../config/db/dbconnection';
 
 export class Server {
     private app : Application;
@@ -7,8 +9,25 @@ export class Server {
     constructor(){
         this.app = express()
         this.port = Number(process.env.PORT) || 3000
+
+        this.db()
+
     }
 
+    middlewares() {
+        this.app.use(cors())
+        this.app.use(express.json())
+    }
+
+    async db() {
+        try {
+            await db.initialize()
+            console.log(`Base de datos iniciada correctamente`);
+        } catch (error) {
+            console.log(`Hubo un problema al iniciar la base de datos`);
+            console.error(error)
+        }
+    }
 
     listen(){
         this.app.listen(this.port, ()=>{
