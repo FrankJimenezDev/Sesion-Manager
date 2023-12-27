4addb97d6560104e1059569accd294ffd1ce74bc
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2023-12-27 18:21:42 -0500

    configuracion de la coneccion a la db actualizada, ahora usa variables de entorno

--- src/config/db/dbconnection.ts
+++ src/config/db/dbconnection.ts
@@ -1,13 +1,14 @@
+import "reflect-metadata"
 import { DataSource } from "typeorm";
 import { Loginsesion } from "../entity/loginsesion.entity";
 
 export const db = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "123456",
-    database: "SesionManager",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     entities: [Loginsesion],
     synchronize: true,
     logging: false,
